2eb466c24e92f48ea376fd3411aea314d82b578a
Marina Ustinovich <marishka0509@mail.ru>
2023-03-19 20:01:28 +0300

    added cursor hammer

--- src/js/cursors.js
+++ src/js/cursors.js
@@ -3,7 +3,7 @@ const cursors = {
   pointer: 'pointer',
   crosshair: 'crosshair',
   notallowed: 'not-allowed',
-  hammer: "url('img/hammer_small.png'), not-allowed",
+  hammer: "url('./img/hammer_small.png'), not-allowed",
 };
 
 export default cursors;
